79915ec97043ea8821a1c28807a2f8d681f4b4bf
jbmominee <jaymominee@gmail.com>
2018-09-08 14:18:39 -0500

    updated library and album components

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,1 +1,13 @@
-Dummy Album Add Add
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return(
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
+
+export default Album;
