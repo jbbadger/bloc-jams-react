ceaf7882ecc485616e300bc4ddb4304cdbb74e05
jbmominee <jaymominee@gmail.com>
2018-09-08 19:19:19 -0500

    fixed render error in album and library js

--- src/components/Album.js
+++ src/components/Album.js
@@ -15,14 +15,14 @@ class Album extends Component {
   }
 
   render() {
-    return(
+    return (
       <section className="album">
         <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.title}/>
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
-            <div id='release=info'>{this.state.album.releaseInfo}</div>
+            <div id="release=info">{this.state.album.releaseInfo}</div>
           </div>
         </section>
         <table id="song-list">
